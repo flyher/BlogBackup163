c4af22af9e9af382d2c52b528a89ccd673be9ded
flyher <dong3580@gmail.com>
2015-04-22 22:36:12 +0800

    update to v0.1

---
+++ BlogBackup163/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BlogBackup163
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtLog = new System.Windows.Forms.RichTextBox();
+            this.btnExquery = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGetCookie = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtxtLog
+            // 
+            this.rtxtLog.Location = new System.Drawing.Point(228, 12);
+            this.rtxtLog.Name = "rtxtLog";
+            this.rtxtLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.rtxtLog.Size = new System.Drawing.Size(199, 360);
+            this.rtxtLog.TabIndex = 1;
+            this.rtxtLog.Text = "1.输入用户名，点击打开(页面会打开网易博客的登录页面)，输入自己的信息登录(代码开源，该程序不经过任何第三方服务器).\n2.登录成功后，点击\"获取登录状态\";\n" +
+    "3.获取成功之后，点击执行备份。会自动在程序目录下新建一个blog文件夹执行备份。";
+            // 
+            // btnExquery
+            // 
+            this.btnExquery.Location = new System.Drawing.Point(446, 198);
+            this.btnExquery.Name = "btnExquery";
+            this.btnExquery.Size = new System.Drawing.Size(100, 23);
+            this.btnExquery.TabIndex = 2;
+            this.btnExquery.Text = "执行备份";
+            this.btnExquery.UseVisualStyleBackColor = true;
+            this.btnExquery.Click += new System.EventHandler(this.btnExquery_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(446, 40);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 21);
+            this.txtUserName.TabIndex = 3;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(199, 360);
+            this.webBrowser1.TabIndex = 5;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(446, 100);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(100, 23);
+            this.btnOpen.TabIndex = 6;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGetCookie
+            // 
+            this.btnGetCookie.Location = new System.Drawing.Point(446, 152);
+            this.btnGetCookie.Name = "btnGetCookie";
+            this.btnGetCookie.Size = new System.Drawing.Size(100, 23);
+            this.btnGetCookie.TabIndex = 7;
+            this.btnGetCookie.Text = "获取登录状态";
+            this.btnGetCookie.UseVisualStyleBackColor = true;
+            this.btnGetCookie.Click += new System.EventHandler(this.btnGetCookie_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "用户名:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 384);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGetCookie);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnExquery);
+            this.Controls.Add(this.rtxtLog);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BlogBackUp163 V0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtLog;
+        private System.Windows.Forms.Button btnExquery;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGetCookie;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ BlogBackup163/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Flyher.Road.Example;
+using HtmlAgilityPack;
+using System.Net;
+using Flyher.Road.Net;
+using System.IO;
+using System.Web;
+using System.Threading;
+
+namespace BlogBackup163
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Blog163 bg = new Blog163();
+        CookieContainer myCookieContainer = new CookieContainer();
+        ConHttp ch = new ConHttp();
+        HtmlPack hp = new HtmlPack();
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Url = new Uri("http://wap.blog.163.com/w2/login.do");
+        }
+
+        private void btnGetCookie_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.Document.Cookie != null)
+            {
+                string cookieStr = webBrowser1.Document.Cookie;
+                string[] cookstr = cookieStr.Split(';');
+                foreach (string str in cookstr)
+                { 
+                    string[] cookieNameValue = str.Split('=');
+                    if (cookieNameValue.Length != 2)
+                    {
+                        Cookie ck = new Cookie(cookieNameValue[0].Trim().ToString(), cookieNameValue[1].Trim().ToString()+"==");
+                        ck.Domain = "wap.blog.163.com";
+                        myCookieContainer.Add(ck);  
+                    }
+                    else
+                    { 
+                        Cookie ck = new Cookie(cookieNameValue[0].Trim().ToString(), cookieNameValue[1].Trim().ToString());
+                        ck.Domain = "wap.blog.163.com";
+                        myCookieContainer.Add(ck);                        
+                    }
+                }
+                Invoke(new Action(() =>
+                {
+                    rtxtLog.AppendText("\n登录状态获取成功!\n");
+                    rtxtLog.Focus();
+                }));
+            }
+        }
+
+        private void btnExquery_Click(object sender, EventArgs e)
+        {
+            string userName = txtUserName.Text.Trim();
+            Thread t1 = new Thread(() => { Exquery(userName); });
+            t1.IsBackground = true;
+            t1.Start();
+        }
+        public void Exquery(string userName)
+        {
+            try
+            {
+                string blogUrl = "http://" + userName + ".wap.blog.163.com/w2/blogList.do?p=1";
+                string html = ch.GetResponseHtml(blogUrl, "get", "", "application/x-www-form-urlencoded", "utf-8", myCookieContainer);
+                HtmlNode hn = hp.ConvertHtml(html);
+
+                int page = bg.PageCount(hn);
+
+                Invoke(new Action(() =>
+                {
+                    rtxtLog.Text += "博客页数:" + page.ToString() + "\n";
+                    rtxtLog.Focus();
+                }));
+                List<string> lst = new List<string>();
+                for (int i = 1; i <= page; i++)
+                {
+                    if (i == 2)
+                    {
+                        string b = "";
+                    }
+                    string urlPage = "http://" + userName + ".wap.blog.163.com/w2/blogList.do?p=" + i.ToString();
+                    string htmlPage = ch.GetResponseHtml(urlPage, "get", "", "application/x-www-form-urlencoded", "utf-8", myCookieContainer);
+                    HtmlNode hnPage = hp.ConvertHtml(htmlPage);
+
+                    int count = bg.ArtCount(hnPage);
+                    for (int j = 1; j <= count; j++)
+                    {
+                        string title = bg.ArtTitle(hnPage, j);
+                        string url = HttpUtility.UrlDecode(bg.ArtUrl(hnPage, j)).Replace("&amp;", "&") + "&showRest=true";
+                        string pushTime = bg.ArtPushTime(hnPage, j);
+                        string readcount = bg.ArtReadCount(hnPage, j);
+                        string commentCount = bg.ArtCommentCount(hnPage, j);
+                        string htmlArt = ch.GetResponseHtml(url, "get", "", "application/x-www-form-urlencoded", "utf-8", myCookieContainer);
+
+                        CreateLogHtmlId("blog",("[" + i.ToString() + "_" + j.ToString() + "]" + "[" + pushTime + "]" + title).Replace(":", "#").Replace("\\", "_").Replace("/", ""), htmlArt);
+                        Invoke(new Action(() =>
+                        {
+                            rtxtLog.AppendText("正在抓取:" + page.ToString() + "(" + i.ToString() + "-" + j.ToString() + ")\n");
+                        }));
+                    }
+
+                }
+            }
+            catch (Exception err)
+            {
+                CreateLogHtmlId("log","error","[message]"+err.Message.ToString()+"[data]"+err.Data.ToString()+"[source]"+err.Source.ToString());
+            }
+            Invoke(new Action(() =>
+            {
+                rtxtLog.AppendText("备份成功！");
+            }));
+        }
+
+        /// <summary>
+        /// 保存获取的html源码
+        /// </summary>
+        public void CreateLogHtmlId(string path,string id, string json)
+        {
+            if (!Directory.Exists(path))
+            {
+                //创建日志文件夹
+                Directory.CreateDirectory(path);
+            }
+            path += "\\" + id.ToString() + ".html";
+            if (!System.IO.File.Exists(path))
+            {
+                System.IO.FileStream fs = System.IO.File.Create(path);
+                fs.Close();
+            }
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(path, true))
+            {
+                if (json != null)
+                {
+                    sw.Write(json);
+                }
+                else
+                {
+                    //sw.Write("Exception is NULL");
+                }
+            }
+
+        }
+    }
+}
