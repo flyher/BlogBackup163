224c8bc95aec7edb1e77c1a1a1e6f34a467f53fd
Flyher D <flyher@users.noreply.github.com>
2018-08-31 11:40:08 +0800

    update version

--- BlogBackup163/Properties/AssemblyInfo.cs
+++ BlogBackup163/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("flyher")]
 [assembly: AssemblyProduct("BlogBackup163")]
-[assembly: AssemblyCopyright("Copyright © flyher 2015-2016")]
+[assembly: AssemblyCopyright("Copyright © flyher 2015-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
